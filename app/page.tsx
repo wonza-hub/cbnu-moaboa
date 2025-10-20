682b91db051c063ad51b4c5c8bf4ffa2bc09b0d8
wonza-hub <wonza8585@gmail.com>
2025-10-21 00:47:02 +0900

    chore: FSD 구조로 변경에 따른 파일 위치 이동

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * PAGE: 홈
+ */
+export default function Home() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start"></main>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]"></footer>
+    </div>
+  );
+}
