85c7c58994666f76f515b2272de5441be6d7135c
wonza-hub <wonza8585@gmail.com>
2025-10-21 00:42:23 +0900

    feat: 공지사항 엔트리 포인트 추가

---
+++ src/entities/notice/index.ts
@@ -0,0 +1,11 @@
+// UI
+export { default as NoticeCard } from "./ui/notice-card";
+export { default as NoticeCardSkeleton } from "./ui/notice-card-skeleton";
+
+// Types
+export type { INoticeRowData } from "./types/notice";
+export type { IApiResponse } from "./types/notice";
+export type { IInfiniteNoticesData } from "./types/notice";
+
+// Constants
+export { NOTICE_GROUPS, CATEGORY_COLORS } from "./lib/constants";
