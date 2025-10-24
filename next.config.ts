fd8960876b22f2ac326336201776607321016728
wonza-hub <wonza8585@gmail.com>
2025-10-25 00:17:52 +0900

    chore: Next Image src에 이미지 url 추가하기 위해 config에 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.cbnu.ac.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "www.cieat.chungbuk.ac.kr",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
