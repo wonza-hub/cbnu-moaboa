e7c29df737a5a8d3a4d4d1ccadf682246fb5a058
wonza-hub <wonza8585@gmail.com>
2025-10-26 14:22:13 +0900

    chore: 진입점 파일 정리

---
+++ src/widgets/notice-detail/index.ts
@@ -0,0 +1,3 @@
+export { default as NoticeDetailDrawer } from "./ui/notice-detail-drawer";
+export { default as NoticeDetailSkeleton } from "./ui/notice-detail-skeleton";
+export { useNoticeDetailDrawerStore } from "../../shared/stores/use-notice-detail-drawer-store";
