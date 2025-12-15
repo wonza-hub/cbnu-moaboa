6d2dbacd55f56e514e4e73fa24b1f1295451bb14
wonza-hub <wonza8585@gmail.com>
2025-12-15 21:14:48 +0900

    feat: 북마크, 마이페이지 추가

---
+++ app/mypage/page.tsx
@@ -0,0 +1,10 @@
+export default function MyPage() {
+  return (
+    <div className="bg-background flex min-h-screen flex-col items-center justify-center gap-8 p-4 pt-20">
+      <div className="z-10 text-center">
+        <h1 className="mb-8 text-2xl font-bold">마이페이지</h1>
+        <div className="flex flex-col items-center justify-center gap-6"></div>
+      </div>
+    </div>
+  );
+}
