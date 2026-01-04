7eaaf986b518362c4178028ba3c2c3e064fe4a73
wonza-hub <wonza8585@gmail.com>
2026-01-04 23:11:33 +0900

    refactor: 인덱스페이지에 metadata 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "충북대학교 통합 공지사항",
+  description: "충북대학교의 다양한 공지사항을 한 곳에서 확인하세요.",
+  openGraph: {
+    title: "충북대학교 통합 공지사항",
+    description: "충북대학교의 다양한 공지사항을 한 곳에서 확인하세요.",
+    images: ["/thumbnail.webp"],
+  },
+};
 
 export default function Page() {
   // TODO: 임시로 공지사항 페이지로 리다이렉트
